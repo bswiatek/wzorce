dd1ee710ce1300b572ba143e8197a81af6a8c508
Bartłomiej Świątek <emini1@swiatek.biz>
2015-06-25 17:40:12 +0200

    zmiany w textProduct

--- metoda_fabrykujaca/TextProduct.php
+++ metoda_fabrykujaca/TextProduct.php
@@ -11,7 +11,29 @@ class TextProduct implements Product
     private $mfgProduct;
     public function getProperties()
     {
-        $this->mfgProduct="To jest tekst";
+        $this->mfgProduct= <<<MALI
+<!doctype html>
+<head>
+<style type="text/css">
+header{
+    color: #900;
+    font-weight: bold;
+    font-size: 24px;
+    font-family: Verdana, Geneva, sans-serif;
+    }
+    p{
+    font-family: Verdana, Geneva, sans-serif;
+    font-size: 12px;
+    }
+</style>
+<meta charset="UTF-8"><title>Mali</title></head>
+<body>
+<p>Mali, Republika Mali (fr. République du Mali, bambara Mali ka Fasojamana) – śródlądowe państwo położone w Afryce Zachodniej, siódme co do wielkości w Afryce ze stolicą Bamako, graniczy z Algierią na północy, Nigrem na wschodzie, Burkiną Faso oraz Wybrzeżem Kości Słoniowej na południu, Gwineą na południowym zachodzie oraz z Senegalem i Mauretanią na zachodzie.
+
+Mali składa się z ośmiu regionów i jego granice na północy sięgają południowej Sahary, podczas gdy południowa część kraju, zamieszkana przez większość społeczeństwa, graniczy z rzekami Niger oraz Senegal. W strukturze gospodarczej kraju przeważa rolnictwo oraz rybołówstwo. Niektóre z bogactw naturalnych Mali to złoto, uran czy sól. Uważa się, że Mali jest jednym z najbiedniejszych państw świata.</p>
+</body></html>
+MALI;
+
         return $this->mfgProduct;
     }
 }
